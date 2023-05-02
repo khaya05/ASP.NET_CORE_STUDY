7604d7c79922a0cf4d5162bfcac2d1be3778dab0
khaya05 <khayamnyandu5@gmail.com>
2023-05-02 08:45:45 +0200

    custom middleware

---
+++ src/MiddleWareExample/CustomMiddleware/MyCustomMiddleware.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MiddleWareExample.CustomMiddleware;
+public class MyCustomMiddleware : IMiddleware
+{
+  public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+  {
+    await context.Response.WriteAsync("My custom middleware - starts");
+    await next(context);
+    await context.Response.WriteAsync("My custom middleware - ends!");
+  }
+}
--- src/MiddleWareExample/Program.cs
+++ src/MiddleWareExample/Program.cs
@@ -1,6 +1,21 @@
+using MiddleWareExample.CustomMiddleware;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddTransient<MyCustomMiddleware>();
 var app = builder.Build();
 
+// middleware 1
+app.Use(async(HttpContext context, RequestDelegate next) =>
+{
+  await context.Response.WriteAsync("Hello");
+  await next(context);
+});
 
+// middleware 2: custom middleware
+app.UseMiddleware<MyCustomMiddleware>();
 
+app.Run(async (HttpContext context) =>
+{
+  await context.Response.WriteAsync("Hey");
+});
 app.Run();
