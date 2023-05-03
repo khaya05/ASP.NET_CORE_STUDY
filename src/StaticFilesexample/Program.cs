1814d281cfeb937dfcb936fd62113230bd024ee5
khaya05 <khayamnyandu5@gmail.com>
2023-05-03 08:06:15 +0200

    bug fix

---
+++ src/StaticFilesexample/Program.cs
@@ -0,0 +1,17 @@
+using System.Runtime.InteropServices;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseEndpoints(endpoints =>
+{
+  endpoints.Map("/", async context =>
+  {
+    await context.Response.WriteAsync("hello");
+  });
+});
+app.Run();
