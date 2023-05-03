28461cad71ef178b424e016b5cc7b0161d6e3283
khaya05 <khayamnyandu5@gmail.com>
2023-05-03 12:01:52 +0200

    controllers example

---
+++ src/ControllersExample/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControllersExample.Controllers
+{
+  public class HomeController
+  {
+    [Route("/")]
+    public string method1()
+    {
+      return "hello from method1";
+    }
+  }
+}
