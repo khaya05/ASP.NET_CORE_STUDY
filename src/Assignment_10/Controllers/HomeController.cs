3fcae1838dc2386de494aa853f6a4cce2555654a
khaya05 <khayamnyandu5@gmail.com>
2023-05-05 13:34:45 +0200

    assignment 10

---
+++ src/Assignment_10/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment_10.Controllers
+{
+  public class HomeController : Controller
+  {
+    [Route("/")]
+    public IActionResult Index()
+    {
+      return Content("Welcome to the Best Bank");
+    }
+
+    [Route("account-details")]
+    public IActionResult AccountDetails()
+    {
+      return Json(new { accountNumber = 1001, accountHolderName = "Don", currentBalance = 5000 });  
+    }
+
+    [Route("account-statement")]
+    public IActionResult AccountStatement() 
+    {
+      return File("/js.pdf", "application/pdf");
+    }
+
+    [Route("get-current-balance/{accno:int}")]
+    public IActionResult AccountBalance()
+    {
+      if (!Request.Query.ContainsKey("accno"))
+      {
+        return NotFound();
+      }
+
+      if (Convert.ToString(Request.Query["accno"]) != "1001")
+      {
+        return BadRequest();
+      }
+      return Content("5000");
+    }
+  }
+}
