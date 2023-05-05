35aa17cddb20a542d4be1df12b4076d41acc744d
khaya05 <khayamnyandu5@gmail.com>
2023-05-05 14:47:05 +0200

    model class

--- src/IActinResultExample/Controllers/HomeController.cs
+++ src/IActinResultExample/Controllers/HomeController.cs
@@ -1,15 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using IActinResultExample.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace IActinResultExample.Controllers
 {
   public class HomeController : Controller
   {
-    [Route("bookstore")]
-    public IActionResult Index()
+    [Route("bookstore/{bookid?}/{isloggedin?}")]
+    public IActionResult Index([FromRoute] int? bookid, bool? isLoggedIn, Book book)
     {
-      if(!Request.Query.ContainsKey("bookId"))
+      if(bookid.HasValue == false)
       {
-        return BadRequest("book id is not supplied");
+        return BadRequest("book id is not supplied or empty");
       }
 
       if (string.IsNullOrEmpty(Convert.ToString(Request.Query["bookId"])))
@@ -17,26 +18,25 @@ namespace IActinResultExample.Controllers
         return BadRequest("book id can't be emplty");
       }
 
-      //book id should be 1 to 1000
-      int id = Convert.ToInt32(Request.Query["bookId"]);
-
-      if(id <= 0)
+      if(bookid <= 0)
       {
         return NotFound("Book id too small");
       }
-      if(id > 1000)
+      if(bookid > 1000)
       {
         return NotFound("id too large");
       }
 
-      if (Convert.ToBoolean(Request.Query["isloggedin"] == false))
+      if (isLoggedIn == false)
       {
         return Unauthorized("User must be authenticated");
       }
 
+      return Content($"Book id: {bookid}, Book: {book}", "text/plain");
+
       // 302
       //return new RedirectToActionResult("Books", "Store", new {});
-      // return RedirectToAction("Books", "Store", new { });
+      //return RedirectToAction("Books", "Store", new { });
 
       // 301 - moved permanently
       // return new RedirectToActionResult("Books", "Store", new { },true);
