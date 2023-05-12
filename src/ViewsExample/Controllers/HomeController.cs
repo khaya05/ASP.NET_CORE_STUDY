d6fcc3a22ab36365149f6f7d5d463af9142e0b0a
khaya05 <khayamnyandu5@gmail.com>
2023-05-12 11:23:08 +0200

    Razor Code blocks

--- src/ViewsExample/Controllers/HomeController.cs
+++ src/ViewsExample/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ namespace ViewsExample.Controllers
   public class HomeController : Controller
   {
     [Route("/")]
+    [Route("home")]
     public IActionResult Index()
     {
       return View();
